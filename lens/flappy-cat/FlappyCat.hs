73b460afab830ecb669d06b537375b9f92168fa2
Tim Baumann <tim@timbaumann.info>
2015-08-05 02:08:28 +0200

    Lens-Präsentation: Flappy Cat fertig

--- lens/flappy-cat/FlappyCat.hs
+++ lens/flappy-cat/FlappyCat.hs
@@ -3,22 +3,57 @@
 module FlappyCat where
 
 import Control.Lens
+import System.Random
+import Control.Monad.State.Lazy (MonadState (..))
 
 -- Constants
 jumpVel, gravity, velX :: Float
-jumpVel = 12
-gravity = -30
-velX = 10
+jumpVel = 200
+gravity = -450
+velX = 150
 width, height :: Int
 width  = 600
 height = 400
 
+sockeExtremePoints :: [(Float, Float)]
+sockeExtremePoints =
+  map (\(x,y) -> (x - 37, 32 - y)) $
+  [ (67,7)
+  , (71,26)
+  , (64,60)
+  , (51,63)
+  , (16,60)
+  , (1,1)
+  ]
+
+data Hurdle =
+  Hurdle
+  { _hurdlePosX :: Float
+  , _hurdleHeight :: Float
+  }
+
+makeLenses ''Hurdle
+
+randomHurdle :: (RandomGen g, MonadState g m) => Float -> m Hurdle
+randomHurdle posX = state $ \g ->
+  let (dist, g')     = randomR (200, 600) g
+      (hHeight, g'') = randomR (-140, 140) g'
+  in (Hurdle (posX + dist) hHeight, g'')
+
+passes :: (Float, Float) -> Hurdle -> Bool
+passes (x, y) hurdle =
+  abs (x - hurdle ^. hurdlePosX) > 25 ||
+  abs (y - hurdle ^. hurdleHeight) < 70
+
 data FlappyCat =
   FlappyCat
-  { _pressed :: Bool
+  { _gen :: StdGen
+  , _pressed :: Bool
   , _gameOver :: Bool
+  , _dist :: Float
   , _velY :: Float
   , _posY :: Float
+  , _hurdles :: [Hurdle]
   }
 
 makeLenses ''FlappyCat
@@ -26,8 +61,11 @@ makeLenses ''FlappyCat
 initialFlappyCat :: FlappyCat
 initialFlappyCat =
   FlappyCat
-  { _pressed = False
+  { _gen = mkStdGen 42
+  , _pressed = False
   , _gameOver = False
+  , _dist = 0
   , _velY = jumpVel
   , _posY = 0
+  , _hurdles = []
   }
--- lens/flappy-cat/Main.hs
+++ lens/flappy-cat/Main.hs
@@ -4,6 +4,7 @@ import Graphics.Gloss
 import Graphics.Gloss.Interface.IO.Game
 import Control.Lens
 import Control.Monad.State.Lazy
+import Data.Maybe (fromMaybe)
 import Paths_flappy_cat
 
 import FlappyCat
@@ -44,8 +45,21 @@ render socke background wfda fc
       let deg = atan (fc ^. velY / velX) * (-180/pi) in
       Pictures
       [ background
-      , Translate (-170) (fc ^. posY) $ Rotate deg socke
+      , Translate (-200) 0 $ Color (dim orange) $
+          Pictures $ fc ^.. hurdles.traverse.to renderHurdle
+      , Translate (-200) (fc ^. posY) $ Rotate deg socke
+      , Color white $ Translate (-290) 170 $ Scale 0.15 0.15 $
+          Text $ show $ round $ fc ^. dist
       ]
+  where
+    renderHurdle hurdle =
+      let x = (hurdle ^. hurdlePosX) - (fc ^. dist)
+          rectPath y1 y2 = [(x-25,y1), (x+25,y1), (x+25,y2), (x-25,y2)]
+          hh = hurdle ^. hurdleHeight
+      in Pictures
+         [ Polygon $ rectPath (-200) (hh - 70)
+         , Polygon $ rectPath (hh + 70) 200
+         ]
 
 handleInput :: Event -> State FlappyCat ()
 handleInput (EventKey (SpecialKey key) state _ _) | key `elem` jumpKeys = do
@@ -55,15 +69,30 @@ handleInput (EventKey (SpecialKey key) state _ _) | key `elem` jumpKeys = do
     velY .= jumpVel
     isGameOver <- use gameOver
     when isGameOver $ do
+      -- Reset
       gameOver .= False
       posY .= 0
+      dist .= 0
+      hurdles .= []
   pressed .= pressedNow
 handleInput _ = return ()
 
 step :: Float -> State FlappyCat ()
 step dt = do
   vy <- velY <+= (dt * gravity)
-  py <- posY <+= vy
+  py <- posY <+= (dt * vy)
   when (py <= bottom) $ gameOver .= True
+  d <- dist <+= dt * velX
+  hs <- hurdles <%= filter ((> (d - 400)) . (^. hurdlePosX))
+  let lastX = fromMaybe (d+600) $ lastOf (traverse.hurdlePosX) hs
+  when (lastX < d+800) $ do
+    hurdle <- zoom gen $ randomHurdle lastX
+    hurdles .= hs ++ [hurdle]
+  let rad = atan (vy / velX)
+      criticalPoints =
+        flip map sockeExtremePoints $ \(x, y) ->
+          (d + cos rad * x + sin rad * y, py - sin rad * x + cos rad * y)
   return ()
+  unless (all (uncurry passes) $ (,) <$> criticalPoints <*> hs) $ do
+    gameOver .= True
   where bottom = negate $ fromIntegral $ height `div` 2
